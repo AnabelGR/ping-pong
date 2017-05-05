942bf76902b2bb3195ab0decc9a6999e3b77fa12
Anabel Ramirez <anabel@apparentinc.com>
2017-05-05 16:38:17 -0700

    add design elements to html, more clean up of code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 // Back-End Logic
 var pingPong = function(number) {
-  for (i = 0; i < number + 1; i++); {
-    if (number === 0) {
-      return ("0");
-    }
-    else if (number % 15 === 0) {
-      return ("P I N G - P O N G !");
-    }
-    else if (number % 5 === 0) {
-      return ("P O N G !");
-    }
-    else if (number % 3 === 0) {
-      return ("P I N G !");
-    }
-    else if (number) {
-      return number;
+  for (i = 0; i < number + 1; i++);
+    { if (number === 0) {
+        return ("0");
+    } else
+        if (number % 15 === 0) {
+        return ("P I N G - P O N G !");
+    } else
+        if (number % 5 === 0) {
+        return ("P O N G !");
+    } else
+        if (number % 3 === 0) {
+        return ("P I N G !");
+    } else
+        if (number) {
+        return number;
     } else {
-      return ("This is not a valid number.");
-      clear;
+        return ("This is not a valid number.");
+        clear;
     }
   }
 };
@@ -25,12 +25,10 @@ var pingPong = function(number) {
 // User Interface Logic
 $(document).ready(function() {
   $("form#user-number").submit(function(event) {
-    event.preventDefault();
+      event.preventDefault();
     var number = parseInt($("input#number").val());
     var result = pingPong(number);
     var clear = $("input#number").val('');
-
-    $("div#results").append("<p class='animated zoomInLeft'>" + result + "</p>");
-    $("div#results").show();
+      $("div#results").append("<p class='animated bounceInUp'>" + result + "</p>");
   });
 });
