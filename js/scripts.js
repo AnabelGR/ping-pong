767e8b5469a18ef75662401fafb54d2fc4365e69
Anabel Ramirez <anabel@apparentinc.com>
2017-05-05 10:20:23 -0700

    format the html with color, fonts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,28 @@
 // Back-End Logic
 var pingPong = function(number) {
   for (i = 1; i < number + 1; i++); {
-  if (number % 15 === 0) {
-    return ("ping-pong");
+    if (number % 15 === 0) {
+      return ("P I N G - P O N G !");
+    }
+    else if (number % 5 === 0) {
+      return ("P O N G !");
+    }
+    else if (number % 3 === 0) {
+      return ("P I N G !");
+    } else {
+      return number;
+    }
   }
-  else if (number % 5 === 0) {
-    return ("pong");
-  }
-  else if (number % 3 === 0) {
-    return ("ping");
-  } else {
-    return number;
-  }
-}
 };
 
 // User Interface Logic
 $(document).ready(function() {
   $("form#user-number").submit(function(event) {
-          event.preventDefault();
+    event.preventDefault();
     var number = parseInt($("input#number").val());
     var result = pingPong(number);
 
     $("ul#results").append("<li>" + result + "</li>");
     $("ul#results").show();
-
   });
 });
