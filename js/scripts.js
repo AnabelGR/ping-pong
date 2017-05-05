af09197e94654c8912d15110e219356771645705
Anabel Ramirez <anabel@apparentinc.com>
2017-05-05 09:17:33 -0700

    divide the input number by 15 to return 'ping-pong' and divide the input number by 5 to return 'pong'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 // Back-End Logic
 var numberPing = function(number) {
-  if (number % 3 === 0) {
+  if (number % 15 === 0) {
+    return ("ping-pong");
+  }
+  else if (number % 5 === 0) {
+    return ("pong");
+  }
+  else if (number % 3 === 0) {
     return ("ping");
+  } else {
+    return number;
   }
 };
 
